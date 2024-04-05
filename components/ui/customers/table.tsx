8643baff0a96fec62be3078223687682e976165f
anthonyBuiDev <thanhnambui201@gmail.com>
2024-04-05 09:25:28 +0700

    fix ui

--- components/ui/customers/table.tsx
+++ components/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/components/ui/fonts';
 import Search from '@/components/ui/search';
 import {
   CustomersTableType,
