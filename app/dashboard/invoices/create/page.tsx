fc201bff267fbc33ed81732a71d63dbf4aeaca34
anthonyBuiDev <thanhnambui201@gmail.com>
2024-04-08 15:05:25 +0700

    final

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,27 @@
+import Form from '@/components/ui/invoices/create-form';
+import Breadcrumbs from '@/components/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
